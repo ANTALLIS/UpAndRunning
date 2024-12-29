a7afc032fdfab6331aed11f34f52c575c048109c
Antonio Allis <antonioallis@hotmail.co.uk>
2024-12-29 18:48:15 +0000

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,19 @@
+sudo apt -y install tmux
+sudo apt -y install htop
+sudo apt -y install git
+# sudo apt -y install cool-retro-term
+# sudo apt -y install neovim
+sudo apt -y install cmake
+sudo apt -y install curl
+sudo apt -y install lsd
+sudo apt -y install neofetch
+sudo apt -y install onefetch
+# install chrome
+# copy config from Github
+# download fonts
+sudo apt -y install postgresql-16
+# install qbittorrent
+# configure GRUB
+# configure disks to automount
+sudo apt -y install valgrind
+sudo apt -y install wget
